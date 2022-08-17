e26732d55b0172784acab1dd58af08ea5a084a87
okasentana <oka.sentana@taksuteknologi.com>
2022-08-17 23:01:40 +0800

    Realtime chat prototype

--- src/routes/chat/index.svelte
+++ src/routes/chat/index.svelte
@@ -2,29 +2,58 @@
 	import { supabase } from '../../supabase';
 	import { onMount } from 'svelte';
 	let test = '';
+	let isLoading = true;
+	let chatMessages = [];
+	let Message;
+
 	onMount(() => {
-		showchat();
+		fetchMessages();
+
+		return () => Message.unsubscribe();
 	});
-	async function handleSubmit() {
-		let username = localStorage.getItem('username');
-		let jurusan = localStorage.getItem('jurusan');
-		const { data, error } = await supabase
-			.from('group_chat')
-			.insert([{ username: username, message: test, group_id: jurusan }]);
+	// async function handleSubmit() {
+	// 	let username = localStorage.getItem('username');
+	// 	let jurusan = localStorage.getItem('jurusan');
+	// 	const { data, error } = await supabase
+	// 		.from('KelasMataPelajaran')
+	// 		.insert([{ username: username, message: test, group_id: jurusan }]);
+	// }
+
+	async function subscribeMessage() {
+		Message = supabase
+			.from(`Message:roomId=eq.6852d9c9-e17d-438f-93fa-e5b824a87e78`)
+			.on('*', (payload) => {
+				console.log('payload recieved', payload);
+				chatMessages = [...chatMessages, payload.new];
+			})
+			.subscribe();
 	}
-	let datas = [];
-	async function showchat() {
-		let jurusan = localStorage.getItem('jurusan');
-		let { data, error } = await supabase.from('group_chat').select('*').eq('group_id', jurusan);
-		datas = data;
+
+	async function fetchMessages() {
+		let { data: messages } = await supabase
+			.from('Message')
+			.select('*')
+			.eq('roomId', '6852d9c9-e17d-438f-93fa-e5b824a87e78');
+		console.log('old messages', messages);
+		chatMessages = messages;
+		subscribeMessage();
+		isLoading = false;
 	}
 </script>
 
 <main>
 	<input type="text" placeholder="input di sini" bind:value={test} />
-	<button on:click={() => handleSubmit()}>submite</button>
+	<!-- <button on:click={() => handleSubmit()}>submite</button> -->
 
-	<div>
-		{datas[0]?.message}
-	</div>
+	{#if isLoading}
+		<p>Loading....</p>
+	{:else}
+		<div>
+			{#each chatMessages as message}
+				<div>{message.text}</div>
+			{:else}
+				<div>No messages...</div>
+			{/each}
+		</div>
+	{/if}
 </main>
