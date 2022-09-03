f5dd2d9519cbba7ef50934e48e530a8d6e5aa728
okasentana <oka.sentana@taksuteknologi.com>
2022-09-03 17:01:58 +0800

    refaktor login form

--- src/routes/auth/index.svelte
+++ src/routes/auth/index.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
+	/* import module */
 	import Login from './login.svelte';
-	import { Card } from 'svelte-materialify';
+	import Card from '@smui/card';
+
 	const rands = [''];
 	const randCls = rands[Math.floor(Math.random() * rands.length)];
 </script>
 
 <section id="body" class={randCls}>
 	<header class="flex justify-center items-center w-full h-screen bg-slate-300">
-		<Card class="rounded-lg shadow-2xl w-[465px] shadow-xs mx-5">
-			<div class="p-5 bg-white">
+		<Card class="w-[460px] max-h-[414px] rounded-none">
+			<div class="p-5">
 				<div class="mb-2">
-					<h1 class="text-2xl font-bold py-2 text-gray-700">Sign In</h1>
-					<span class="text-gray-500">
-						Dont have an account? <span class="text-teal-500">Contact administrator</span>
+					<h1 class="text-2xl font-bold leading-9 text-slate-500">Sign In</h1>
+					<span class="text-xs font-normal leading-5">
+						Dont have an account? <span class="text-teal-400">Contact administrator</span>
 					</span>
 				</div>
 				<Login />
--- src/routes/auth/login.svelte
+++ src/routes/auth/login.svelte
@@ -1,22 +1,73 @@
 <script lang="ts">
-	import { TextField, Button, Icon, Checkbox } from 'svelte-materialify';
-	import { mdiEyeOff, mdiEye } from '@mdi/js';
+	/* Module */
+	import * as yup from 'yup';
+	import { Form, Message, isInvalid } from 'svelte-yup';
 	import jwt_decode from 'jwt-decode';
 	import { variables } from '$lib/variables';
+	import FormField from '@smui/form-field';
+	import Checkbox from '@smui/checkbox';
+	import Button, { Label } from '@smui/button';
+	import HelperText from '@smui/textfield/helper-text';
+	import { Icon } from 'svelte-materialify';
+	import IconButton from '@smui/icon-button';
+	import { mdiEyeOff, mdiEye } from '@mdi/js';
+	import Textfield from '@smui/textfield';
 
+	/* variable */
+	let checked = false;
 	let show = false;
-	let group = ['2'];
 	let username = '';
 	let password = '';
-	let isLoading = false;
-	$: value = '';
-	let error = null;
+
+	/* validation function */
+	let schema = yup.object().shape({
+		username: yup
+			.number()
+			.required()
+			.min(8)
+			.label('Username')
+			.nullable(true)
+			.transform((v, o) => (o === '' ? null : v)),
+		// email: yup.string().required().email().label('Email address'),
+		password: yup
+			.number()
+			.required()
+			.min(18)
+			.label('password')
+			.nullable(true)
+			.transform((v, o) => (o === '' ? null : v))
+		// answer: yup
+		// 	.number()
+		// 	.required()
+		// 	.positive()
+		// 	.oneOf([6], 'Answer is wrong')
+		// 	.label('Answer')
+		// 	.nullable(true)
+		// 	.transform((v, o) => (o === '' ? null : v)),
+		// gender: yup.string().required().label('Gender')
+	});
+	let fields = { username: '', password: '' };
+	let submitted = false;
+	let isValid;
+	function formSubmit() {
+		submitted = true;
+		isValid = schema.isValidSync(fields);
+		if (isValid) {
+			handleSubmit();
+		}
+	}
+	$: invalid = (username) => {
+		if (submitted) {
+			return isInvalid(schema, username, fields);
+		}
+		return false;
+	};
 
 	async function handleSubmit() {
 		const response = await fetch(`${variables.basePath}/signin`, {
 			method: 'POST',
 			credentials: 'same-origin',
-			body: JSON.stringify({ username, password }),
+			body: JSON.stringify({ ...fields }),
 			headers: {
 				'Content-Type': 'application/json'
 			}
@@ -25,11 +76,9 @@
 		if (response.status === 200) {
 			let body = await response.json();
 			localStorage.setItem('token', body.token);
-
 			var decoded: any = jwt_decode(body.token);
 			localStorage.setItem('username', decoded.username);
 			localStorage.setItem('jurusan', decoded.jurusan);
-			isLoading = true;
 
 			if (decoded.role === 'PENGGUNA') {
 				window.location.href = '/chat';
@@ -42,74 +91,41 @@
 	}
 </script>
 
-<section>
-	<form>
-		<div class="relative py-3">
-			<TextField
-				dense
-				filled
-				bind:value={username}
-				rules={[
-					(v) => !!v || 'Required',
-					(v) => v.length <= 10 || 'Max 10 characters',
-					(v) => {
-						const pattern = /^[0-9]*$/;
-						return pattern.test(v) || 'Invalid username.';
-					}
-				]}>Username</TextField
-			>
-		</div>
-		<div class="relative py-3">
-			<TextField
-				filled
-				dense
-				type={show ? 'text' : 'password'}
-				bind:value={password}
-				rules={[(v) => !!v || 'Required']}
-			>
-				Password
-				<div
-					slot="append"
-					on:click={() => {
-						show = !show;
-					}}
-					class="text-teal-500"
-				>
-					<Icon path={show ? mdiEyeOff : mdiEye} />
+<Form class="form" {schema} {fields} submitHandler={formSubmit} {submitted} color="#b00020">
+	<div class="mb-6">
+		<Textfield
+			variant="filled"
+			label="Username"
+			type="text"
+			bind:value={fields.username}
+			invalid={invalid('username')}
+		/>
+		<Message name="username" />
+	</div>
+	<div class="mb-6">
+		<Textfield
+			variant="filled"
+			label="Password"
+			bind:value={fields.password}
+			invalid={invalid('password')}
+			type={show ? 'text' : 'password'}
+		>
+			<IconButton class="material-icons" slot="trailingIcon" on:click={() => (show = !show)}>
+				<div class="flex items-center text-sm">
+					<Icon path={show ? mdiEye : mdiEyeOff} />
 				</div>
-			</TextField>
-			<div class="mt-3">
-				<span>
-					<a class="text-gray-600 hover:underline hover:text-teal-500" href="./forgot-password">
-						Forgot your password?
-					</a>
-				</span>
-			</div>
-		</div>
-		<div class="text-gray-600">
-			<Checkbox
-				bind:group
-				value="1"
-				type="password"
-				name="password"
-				autocomplete="off"
-				class="bg-transparent">Remember me</Checkbox
-			>
-		</div>
-		<div class="cursor-pointer py-4">
-			<Button
-				size="large"
-				class="w-full bg-teal-800 hover:bg-teal-700 white-text"
-				on:click={() => handleSubmit()}
-			>
-				<span class="normal-case">
-					{#if isLoading}
-						loading...
-					{:else}
-						Login
-					{/if}
-				</span>
-			</Button>
-		</div>
-	</form>
-</section>
+			</IconButton>
+		</Textfield>
+		<Message name="password" />
+	</div>
+	<div class="text-xs font-normal text-teal-400 mt-2">Forgot password?</div>
+	<FormField class="flex gap-1 mt-4">
+		<Checkbox bind:checked />
+		<span slot="label" class=" text-xs">Remember me.</span>
+	</FormField>
+	<div class="mt-4">
+		<Button color="secondary" class="w-full h-[45px] normal-case bg-teal-700" variant="raised">
+			<Label>Sign In</Label>
+		</Button>
+	</div>
+</Form>
